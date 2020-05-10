a14157ebb2451f8789fd33f740ce78fe447b16bc
Tom Schneider <tom.schneider-github@sutomaji.net>
2020-05-10 13:51:10 +0200

    First not implemented type

--- server.py
+++ server.py
@@ -7,7 +7,11 @@ import logging
 class ClientConnectorServicer(client_grpc.ClientConnectorServicer):
     def SendMessage(self, request, context):
         print(request)
-        return client.SendMessageResponse(status=client.SendMessageStatus.OK)
+
+        if(request.content.HasField("text_message")):
+            return client.SendMessageResponse(status=client.SendMessageStatus.OK)   
+
+        return client.SendMessageResponse(status=client.SendMessageStatus.MESSAGE_CONTENT_NOT_IMPLEMENTED_FOR_SERVICE)
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
