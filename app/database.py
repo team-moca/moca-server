88496e0ea25d7628def4fa4aa3ecb63c581224a2
Tom Schneider <tom.schneider-github@sutomaji.net>
2021-06-30 00:55:51 +0200

    Autoflush to allow merge to function correctly

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./moca.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
 
 Base = declarative_base()
 Base.metadata.bind = engine
